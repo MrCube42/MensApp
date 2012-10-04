878bf050d7f1e2f7678eda2b11cd5b84c296937d
David Würfel <david@devinmotion.de>
2012-10-04 22:23:39 +0200

    add eclipse environment (phonegap, android sdk stuff, ...)
    * add eclipse environment (phonegap, android sdk stuff, ...)
    * fix white/black button bug in css
    * adjust gitignore

---
+++ src/de/devinmotion/mensapp/MainActivity.java
@@ -0,0 +1,21 @@
+package de.devinmotion.mensapp;
+
+import org.apache.cordova.DroidGap;
+
+import android.os.Bundle;
+import android.view.Menu;
+
+public class MainActivity extends DroidGap {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.loadUrl("file:///android_asset/www/index.html");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+}
