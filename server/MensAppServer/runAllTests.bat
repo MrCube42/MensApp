eeb764abeb245a814b9fc0e720122a21426a9fa3
devinmotion <david@devinmotion.de>
2015-10-28 09:49:58 +0100

    adjust verbosity, add coverage reports and prerequisites info

--- server/MensAppServer/runAllTests.bat
+++ server/MensAppServer/runAllTests.bat
@@ -1,2 +1,4 @@
-green
+echo Start runinng all tests and code coverage with 'green' & echo -- Prerequisites -- & echo (pip install green) & echo (pip install converage)
+
+green -vvv --run-coverage
 pause
