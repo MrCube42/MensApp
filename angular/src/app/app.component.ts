a86074bc9456964522bcc5854d0135f4f6123f7e
David Würfel <david.wuerfel@centigrade.de>
2019-05-30 19:23:26 +0200

    feat: use date-fns, add sample dates and range to view

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { addDays, startOfWeek } from 'date-fns';
+import { Observable, of } from 'rxjs';
+import { map } from 'rxjs/operators';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'mensapp';
+export class AppComponent implements OnInit {
+  public readonly appTitle = 'MensApp';
+  public startDate$: Observable<Date>;
+  public endDate$: Observable<Date>;
+
+  ngOnInit() {
+    const now = new Date();
+    this.startDate$ = of(startOfWeek(now, { weekStartsOn: 1 }));
+    this.endDate$ = this.startDate$.pipe(map(startDate => addDays(startDate, 4)));
+  }
 }
